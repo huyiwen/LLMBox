c12d3021f9ec0bae32be905e028cd05d4be71ffc
huyiwen <1020030101@qq.com>
2024-03-17 15:47:52 +0800

    Add get_prob for anthropic

--- llmbox/model/anthropic.py
+++ llmbox/model/anthropic.py
@@ -1,5 +1,6 @@
 import time
 from logging import getLogger
+from typing import List, Literal, Tuple
 
 import anthropic
 import tiktoken
@@ -51,7 +52,7 @@ class Anthropic(Model):
                 generation_kwargs[key] = value
         self.generation_kwargs = generation_kwargs
 
-    def generation(self, batched_inputs):
+    def generation(self, batched_inputs: List[str]):
         results = self.request(batched_inputs, self.generation_kwargs)
         answers = []
         for result in results:
@@ -59,7 +60,53 @@ class Anthropic(Model):
             answers.append(answer)
         return answers
 
-    def request(self, prompt, kwargs):
+    def set_prob_args(self, **extra_model_args):
+
+        self._word_label_texts = []
+        self._token_label_texts = []
+        if extra_model_args.pop("candidate_ids", None) is not None:
+            logger.warning(f"Anthropic does not support candidate_ids currently, so it will be ignored.")
+
+        self.prob_kwargs = {"max_tokens": 1, "temperature": 0.0}
+
+        if len(extra_model_args) > 0:
+            logger.warning(f"Unused generation arguments: {extra_model_args}")
+
+    def _get_label_texts(self, option_num: int) -> List[str]:
+        """Return the tokenized labels of options and labels themselves."""
+        if len(self._word_label_texts) < option_num:
+            for i in range(len(self._word_label_texts), option_num):
+                self._word_label_texts.append(chr(i + 65))
+                self._token_label_texts.append(" " + chr(i + 65))
+        return self._word_label_texts[:option_num] + self._token_label_texts[:option_num]
+
+    def get_prob(
+        self,
+        batched_inputs: List[Tuple[str, int]],
+        use_logit_bias: Literal[False] = False,
+        return_real_logprobs: Literal[False] = False
+    ) -> List[List[int]]:
+
+        if use_logit_bias or return_real_logprobs:
+            logger.warning("Anthropic does not support logit_bias and logprobs currently, so it will be ignored.")
+
+        *batched_prompts, batched_option_nums = map(list, zip(*batched_inputs))
+        batch_size = len(batched_prompts[0])
+        batched_prompts = ["".join(group[idx] for group in batched_prompts) for idx in range(batch_size)]
+        label_texts = [self._get_label_texts(option_num) for option_num in batched_option_nums]
+
+        results = self.request(batched_prompts, self.prob_kwargs)
+
+        answers = []
+        for result, option_num, label in zip(results, batched_option_nums, label_texts):
+            probs = [-9999.] * (option_num * 2)
+            text = result[0].content[0].text
+            if text in label:
+                probs[label.index(text)] = 20.0
+            answers.append(probs)
+        return answers
+
+    def request(self, prompt, kwargs) -> List[List[anthropic.types.Message]]:
         r"""Call the Anthropic API.
 
         Args:
@@ -73,7 +120,7 @@ class Anthropic(Model):
         for _ in range(self.max_try_times):
             try:
                 message = [{"role": "user", "content": prompt[0]}]
-                response = client.beta.messages.create(model=self.name, messages=message, **kwargs)
+                response = client.messages.create(model=self.name, messages=message, **kwargs)
                 return [[response]]
             except anthropic.RateLimitError:
                 logger.warning("Receive anthropic.RateLimitError, retrying...")
--- llmbox/model/enum.py
+++ llmbox/model/enum.py
@@ -2,4 +2,8 @@ OPENAI_COMPLETION_MODELS = ["babbage-002", "davinci-002", "gpt-3.5-turbo-instruc
 OPENAI_CHAT_MODELS = ["gpt-3.5-turbo", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-4"]
 OPENAI_MODELS = OPENAI_COMPLETION_MODELS + OPENAI_CHAT_MODELS
 OPENAI_INSTRUCTION_MODELS = ["gpt-3.5-turbo-instruct"] + OPENAI_CHAT_MODELS
-ANTHROPIC_MODELS = ["claude-2.1", "claude-instant-1.2"]
+
+# https://docs.anthropic.com/claude/docs/models-overview
+ANTHROPIC_CLAUDE3_MODELS = ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-opus-20240229"]
+ANTHROPIC_LEGACY_MODELS = ["claude-2.1", "claude-instant-1.2"]
+ANTHROPIC_MODELS = ANTHROPIC_CLAUDE3_MODELS + ANTHROPIC_LEGACY_MODELS
