88f30c6b9a39873e58887fa26788d5965f93d2d0
huyiwen <1020030101@qq.com>
2024-05-09 11:17:42 +0800

    Support MultipleChoiceDataset for API models

--- utilization/model/enum.py
+++ utilization/model/enum.py
@@ -1,11 +1,393 @@
-OPENAI_COMPLETION_MODELS = ["babbage-002", "davinci-002", "gpt-3.5-turbo-instruct"]
-OPENAI_CHAT_MODELS = ["gpt-3.5-turbo", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-16k", "gpt-4"]
-OPENAI_MODELS = OPENAI_COMPLETION_MODELS + OPENAI_CHAT_MODELS
-OPENAI_INSTRUCTION_MODELS = ["gpt-3.5-turbo-instruct"] + OPENAI_CHAT_MODELS
-ANTHROPIC_MODELS = ["claude-2.1", "claude-instant-1.2"]
-DASHSCOPE_MODELS = ["qwen-turbo", "qwen-plus", "qwen-max", "qwen-max-1201", "qwen-max-longcontext",
-                    "qwen1.5-72b-chat", "qwen1.5-14b-chat", "qwen1.5-7b-chat", "qwen-72b-chat", "qwen-14b-chat",
-                    "qwen-7b-chat", "qwen-1.8b-longcontext-chat", "qwen-1.8b-chat"]
-QIANFAN_MODELS = ["ernie-4.0-8k", "ernie-3.5-8k", "ernie-3.5-8k-0205", "ernie-3.5-8k-1222",
-                  "ernie-3.5-4k-0205", "ernie-speed-8k", "ernie-speed-128k", "ernie-lite-8k-0922",
-                  "ernie-lite-8k-0308", "ernie tiny", "ernie speed-appbuilder"]
+ANTHROPIC_CHAT_COMPLETIONS_ARGS = {"max_tokens", "stop", "system", "temperature", "top_k", "top_p"}
+ANTHROPIC_CHAT_COMPLETIONS_ALIASES = {
+    "stop": "stop_sequences",
+}
+ANTHROPIC_CHAT_COMPLETIONS_DEFAULTS = {
+    "max_tokens": 4096,
+}
+
+DASHSCOPE_CHAT_COMPLETIONS_ARGS = {
+    "temperature", "top_p", "top_k", "max_tokens", "repetition_penalty", "enable_search", "stop"
+}
+DASHSCOPE_CHAT_COMPLETIONS_DEFAULTS = {
+    "max_tokens": 1024,
+    "temperature": 0.0001,  # can't be 0
+}
+
+OPENAI_CHAT_COMPLETIONS_ARGS = {
+    "frequency_penalty", "logit_bias", "logprobs", "top_logprobs", "max_tokens", "n", "presence_penalty", "seed",
+    "stop", "temperature", "top_p", "best_of"
+}
+OPENAI_CHAT_COMPLETIONS_DEFAULTS = {
+    "max_tokens": 4096,
+}
+
+OPENAI_COMPLETIONS_ARGS = {
+    "best_of", "echo", "frequency_penalty", "logit_bias", "logprobs", "max_tokens", "n", "presence_penalty", "seed",
+    "stop", "temperature", "top_p"
+}
+OPENAI_COMPLETIONS_DEFAULTS = {
+    "max_tokens": 1024,
+}
+
+QIANFAN_CHAT_COMPLETIONS_ARGS = {
+    "temperature", "top_p", "top_k", "penalty_score", "stop", "disable_search", "enable_citation", "max_tokens"
+}
+QIANFAN_CHAT_COMPLETIONS_ALIASES = {
+    "max_tokens": "max_output_tokens",
+}
+QIANFAN_CHAT_COMPLETIONS_DEFAULTS = {
+    "max_tokens": 1024,
+    "temperature": 0.0001,  # can't be 0
+}
+
+ENDPOINT_ARGS = {
+    "dashscope/chat/completions": (
+        DASHSCOPE_CHAT_COMPLETIONS_ARGS,
+        {},
+        DASHSCOPE_CHAT_COMPLETIONS_DEFAULTS,
+    ),
+    "anthropic/chat/completions": (
+        ANTHROPIC_CHAT_COMPLETIONS_ARGS,
+        ANTHROPIC_CHAT_COMPLETIONS_ALIASES,
+        ANTHROPIC_CHAT_COMPLETIONS_DEFAULTS,
+    ),
+    "openai/chat/completions": (
+        OPENAI_CHAT_COMPLETIONS_ARGS,
+        {},
+        OPENAI_CHAT_COMPLETIONS_DEFAULTS,
+    ),
+    "openai/completions": (
+        OPENAI_COMPLETIONS_ARGS,
+        {},
+        OPENAI_COMPLETIONS_DEFAULTS,
+    ),
+    "qianfan/chat/completions": (
+        QIANFAN_CHAT_COMPLETIONS_ARGS,
+        QIANFAN_CHAT_COMPLETIONS_ALIASES,
+        QIANFAN_CHAT_COMPLETIONS_DEFAULTS,
+    ),
+}
+
+API_MODELS = {
+    "babbage-002": {
+        "endpoint": "completions",
+        "model_type": "base",
+        "model_backend": "openai"
+    },
+    "davinci-002": {
+        "endpoint": "completions",
+        "model_type": "base",
+        "model_backend": "openai"
+    },
+    "gpt-3.5-turbo-instruct": {
+        "endpoint": "completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-3.5-turbo-0125": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-3.5-turbo": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-3.5-turbo-1106": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-3.5-turbo-16k": {
+        "endpoint": "completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-3.5-turbo-0613": {
+        "endpoint": "completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-3.5-turbo-16k-0613": {
+        "endpoint": "completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-turbo": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-turbo-2024-04-09": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-turbo-preview": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-0125-preview": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-1106-preview": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-vision-preview": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-1106-vision-preview": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-0613": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-32k": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "gpt-4-32k-0613": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "openai"
+    },
+    "claude-3-opus-20240229": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "anthropic"
+    },
+    "claude-3-sonnet-20240229": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "anthropic"
+    },
+    "claude-3-haiku-20240307": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "anthropic"
+    },
+    "claude-2.1": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "anthropic"
+    },
+    "claude-2.0": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "anthropic"
+    },
+    "claude-instant-1.2": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "anthropic"
+    },
+    "qwen-turbo": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-plus": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-max": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-max-0403": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-max-0107": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-max-longcontext": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-max-0428": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen1.5-110b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen1.5-72b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen1.5-32b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen1.5-14b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen1.5-7b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen1.5-1.8b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen1.5-0.5b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "codeqwen1.5-7b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-72b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-14b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-7b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-1.8b-longcontext-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "qwen-1.8b-chat": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-4.0-8k": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-4.0-8k-preview": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-4.0-8k-0329": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-4.0-8k-0104": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-3.5-8k": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-3.5-8k-0205": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-3.5-8k-1222": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-3.5-4k-0205": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-3.5-8k-preview": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-3.5-8k-0329": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-speed-8k": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-speed-128k": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-lite-8k-0922": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-lite-8k-0308": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-tiny-8k": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-char-8k": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ernie-func-8k": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+    "ai_apaas": {
+        "endpoint": "chat/completions",
+        "model_type": "instruction",
+        "model_backend": "dashscope"
+    },
+}
--- utilization/model/model.py
+++ utilization/model/model.py
@@ -1,8 +1,15 @@
+import time
+from http import HTTPStatus
 from logging import getLogger
 from typing import TYPE_CHECKING, Any, Dict, List, Literal, Optional, Tuple, Union
 
+import tiktoken
 from tiktoken import Encoding
 from transformers import PreTrainedModel, PreTrainedTokenizer, PreTrainedTokenizerFast
+from transformers.pipelines.conversational import Conversation
+
+from utilization.model.enum import API_MODELS, ENDPOINT_ARGS
+from utilization.utils import ModelArguments
 
 from ..utils.arguments import ModelBackendMixin
 from ..utils.prefix_caching import Cacher
@@ -29,14 +36,14 @@ class Model(ModelBackendMixin):
         generation_kwargs (dict): The configurations for open-ended generation.
         ppl_kwargs (dict, *optional*): The configurations for computing PPL score.
     """
-    name = ""
     model_backend: Literal["anthropic", "dashscope", "huggingface", "openai", "qianfan", "vllm"]
 
     model: Union[PreTrainedModel, "LLM", None] = None
-    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast, Encoding]
+    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast, Encoding, None]
     cacher: Optional["Cacher"] = None
     model_max_input_and_output: int
     support_cache: bool = True
+    multi_turn = False
 
     def __init__(self, args: "ModelArguments"):
         self.args = args
@@ -121,3 +128,210 @@ class Model(ModelBackendMixin):
             if getattr(self, key, None):
                 kwargs[key] = getattr(self, key)
         return kwargs
+
+
+ERROR_OVERVIEW = {
+    "APIConnectionError":
+    "Cause: Issue connecting to our services.\nSolution: Check your network settings, proxy configuration, SSL certificates, or firewall rules.",
+    "APITimeoutError":
+    "Cause: Request timed out.\nSolution: Retry your request after a brief wait and contact us if the issue persists.",
+    "AuthenticationError":
+    "Cause: Your API key or token was invalid, expired, or revoked.\nSolution: Check your API key or token and make sure it is correct and active. You may need to generate a new one from your account dashboard.",
+    "BadRequestError":
+    "Cause: Your request was malformed or missing some required parameters, such as a token or an input.\nSolution: The error message should advise you on the specific error made. Check the documentation for the specific API method you are calling and make sure you are sending valid and complete parameters. You may also need to check the encoding, format, or size of your request data.",
+    "ConflictError":
+    "Cause: The resource was updated by another request.\nSolution: Try to update the resource again and ensure no other requests are trying to update it."
+    "InternalServerError"
+    "Cause: Issue on our side.\nSolution: Retry your request after a brief wait and contact us if the issue persists."
+    "NotFoundError"
+    "Cause: Requested resource does not exist.\nSolution: Ensure you are the correct resource identifier.",
+    "PermissionDeniedError":
+    "Cause: You don't have access to the requested resource.\nSolution: Ensure you are using the correct API key, organization ID, resource ID and are not accessing from restricted areas.",
+    "RateLimitError":
+    "Cause: You have hit your assigned rate limit.\nSolution: Pace your requests. Read more in our Rate limit guide.",
+    "UnprocessableEntityError":
+    "Cause: Unable to process the request despite the format being correct.\nSolution: Please try the request again.",
+}
+
+
+class ApiModel(Model):
+
+    model_backend: Literal["anthropic", "dashscope", "openai", "qianfan"]
+
+    model = None
+    tokenizer: Encoding
+    cacher = None
+    model_max_input_and_output: int
+    support_cache: bool = False
+
+    max_retry_times: int = 3
+
+    endpoint: Literal["completions", "chat/completions"]
+
+    _retry_errors: Tuple[Exception] = None
+    _raise_errors: Tuple[Exception] = None
+    _skip_errors: Tuple[Exception] = None
+
+    def __init__(self, args: ModelArguments):
+        super().__init__(args)
+        try:
+            self.tokenizer = tiktoken.get_encoding(args.tokenizer_name_or_path)
+        except Exception as e:
+            logger.warning(
+                f"Failed to load tokenizer from `{args.tokenizer_name_or_path}`. Please specify a valid tokenizer through `--tokenizer`."
+            )
+        if args.model_name_or_path in API_MODELS:
+            self.endpoint = API_MODELS[args.model_name_or_path]['endpoint']
+        else:
+            self.endpoint = "chat/completions"
+
+    def _completions(self, **kwargs):
+        raise NotImplementedError(f"{self.name} model must implement the `_completions` function.")
+
+    def _chat_completions(self, **kwargs):
+        raise NotImplementedError(f"{self.name} model must implement the `_chat_completions` function.")
+
+    @staticmethod
+    def _get_assistant(msg: Any) -> str:
+        raise NotImplementedError(f"Model must implement the `_get_assistant` function.")
+
+    @staticmethod
+    def _get_error_type(response: Any) -> Optional[Exception]:
+        return None
+
+    def set_generation_args(self, **extra_model_args):
+        r"""Set the configurations for open-ended generation. This is useful because different datasets may have different requirements for generation."""
+        generation_kwargs = {}
+        endpoint_name = self.model_backend + "/" + self.endpoint
+        if endpoint_name not in ENDPOINT_ARGS:
+            raise ValueError(f"Endpoint {endpoint_name} is not supported.")
+        endpoint_args = ENDPOINT_ARGS[endpoint_name]
+        endpoint_args, endpoint_aliases, endpoint_default = endpoint_args
+
+        for key in endpoint_args:
+            # ModelArguments (cmd) > extra_model_args > ModelArguments (default)
+            if not self.args.passed_in_commandline(key):
+                value = extra_model_args.pop(key, None)
+            if value is None:
+                value = getattr(self.args, key, None)
+
+            # set default values
+            if key in endpoint_default:
+                if value is None:
+                    value = endpoint_default[key]
+                # special case for temperature
+                elif value == 0 and key == "temperature":
+                    value = endpoint_default
+
+            # set alias after default values
+            if key in endpoint_aliases:
+                key = endpoint_aliases[key]
+
+            if value is not None:
+                generation_kwargs[key] = value
+
+        self.generation_kwargs = generation_kwargs
+        self.multi_turn = extra_model_args.pop("multi_turn", False)
+
+        if len(extra_model_args) > 0:
+            logger.warning(f"Unused generation arguments: {extra_model_args}")
+        return self.generation_kwargs
+
+    def generation(self, batched_inputs) -> Union[List[str], List[Tuple[str, ...]]]:
+        multi_turn_results = self.request(
+            prompt=batched_inputs,
+            multi_turn=self.multi_turn,
+            **self.generation_kwargs,
+        )
+        answers = [self._get_assistant(turn) for turn in multi_turn_results]
+
+        # group the multi-turn results into a tuple
+        if self.multi_turn:
+            answers = [tuple(answers)]
+        return answers
+
+    def request(
+        self,
+        prompt: Union[List[str], List[Conversation]],
+        *,
+        multi_turn: bool = False,
+        **model_kwargs,
+    ) -> List[Any]:
+        r"""Call the OpenAI API.
+
+        Args:
+            prompt (List[str]): The list of input prompts.
+            openai_kwargs (dict): The additional calling configurations.
+            multi_turn (bool): Default is False. Set to True if multi-turns needed.
+
+        Returns:
+            List[Any]: A batch of results (completions endpoint) or a list of conversation (chat/completions endpoint).
+        """
+        retry_times = 0
+        while retry_times < self.max_retry_times:
+            try:
+                if self.endpoint == "completions":
+                    assert not multi_turn
+                    return self._completions(messages=prompt, model=self.name, **model_kwargs)
+                elif self.endpoint == "chat/completions":
+                    assert len(prompt) == 1, "Chat completions does not support batch input."
+                    prompt = prompt[0]
+
+                    # legacy support for string prompt
+                    if isinstance(prompt, str):
+                        messages = [{"role": "user", "content": c} for c in prompt.split("__SEPARATOR__")]
+                        prompt = Conversation(messages)
+
+                    user_idx = 1
+                    for user_idx in range(1, len(prompt.messages)):
+                        if prompt.messages[user_idx]["role"] == "user" and\
+                                prompt.messages[user_idx - 1]["role"] == "user":
+                            assert multi_turn, "Multi-turn conversation is needed."
+                            break
+
+                    num_turns = len(prompt.messages) - user_idx + 1
+                    assert all(
+                        m["role"] == "user" for m in prompt.messages[user_idx - 1:]
+                    ), "The last messages should be user's message."
+
+                    max_messages_with_reply = len(prompt.messages) + num_turns
+                    results = []
+                    for i in range(user_idx, max_messages_with_reply, 2):
+                        response = self._chat_completions(messages=prompt.messages[:i], model=self.name, **model_kwargs)
+
+                        # dashscope compatibility
+                        error_type = self._get_error_type(response)
+                        if error_type is not None:
+                            raise error_type(response.message)
+
+                        results.append(response)
+                        content = self._get_assistant(response)
+                        prompt.messages.insert(i, {
+                            "role": "assistant",
+                            "content": content,
+                        })
+                    return results
+
+                # reset retry_times counter
+                retry_times = 0
+            except self._retry_errors as e:
+                logger.warning(f"Receive {e.__class__.__name__}: {str(e)}, retrying...")
+                retry_times += 1
+                time.sleep(10)
+
+            except self._raise_errors as e:
+                error_msg = e.__class__.__name__ + ": " + str(e)
+                if e.__class__.__name__ in ERROR_OVERVIEW:
+                    error_msg += "\n" + ERROR_OVERVIEW.get(e.__class__.__name__)
+                raise ConnectionError(error_msg)
+
+            except self._skip_errors as e:
+                logger.warning(f"Receive {e.__class__.__name__}: {str(e)}, skipping...")
+                return [""]
+
+            except Exception as e:
+                logger.warning(f"Receive {e.__class__.__name__}: {str(e)}, retrying...")
+                retry_times += 1
+                time.sleep(retry_times)
+
+        raise ConnectionError(f"API endpoint error after {self.max_retry_times} times of retries.")
